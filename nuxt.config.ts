ab2bf3a7c4958bea51c0a3d6c63b8c08d6e14358
996wuxian <1640551913@qq.com>
2025-01-17 15:11:06 +0800

    build docs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   ],
   css: ["~/assets/sass/global.scss"],
   app: {
-    baseURL: "/",
+    baseURL: "/blog.github.io/",
     head: {
       title: "wuxian's blog",
       link: [
