9b6cd8f56ba97020f75678bb0490579c2843390e
996wuxian <1640551913@qq.com>
2025-01-20 10:21:18 +0800

    update header

--- components/app/header.vue
+++ components/app/header.vue
@@ -7,11 +7,9 @@ const timeOfLastClick = useTimeOfLastClick();
 
 <template>
   <header class="z-[9999] fixed top-5 left-0 right-0" data-aos="fade-down">
-    <!-- 背景层 -->
     <div
       class="w-full px-4 sm:px-0 rounded-none border border-white border-opacity-40 bg-white bg-opacity-40 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.2rem] sm:w-[31rem] sm:mx-auto sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
     >
-      <!-- 导航栏 -->
       <nav
         class="flex h-12 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 w-full justify-center"
       >
